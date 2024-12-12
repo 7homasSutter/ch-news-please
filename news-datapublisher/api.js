df5803540cf2342e094f36191cd0783124e4282a
Kaufmann Dario <dario.kaufmann@students.unibe.ch>
2024-12-12 17:32:37 +0100

    improve geocoordinates

--- news-datapublisher/api.js
+++ news-datapublisher/api.js
@@ -1,9 +1,14 @@
 import {getArticlesByQuery} from "./persistenceService.js";
+import {improveLocation} from "./locationService.js";
 
 export async function getAllLocationArticles(ctx) {
     try {
         const query = ctx.request.query
         const articles = await getArticlesByQuery(query)
+        for (const index in articles) {
+            articles[index] = await improveLocation(articles[index])
+        }
+        //articles.forEach(article => console.log(article))
         ctx.body = transformArticles(articles)
     } catch (error) {
         console.log(error)
--- news-datapublisher/index.js
+++ news-datapublisher/index.js
@@ -1,6 +1,7 @@
 import Koa from 'koa'
 import cors from "@koa/cors"
 import {router} from "./routes.js";
+import {connectRedis, initializeData} from "./redisService.js";
 
 const app = new Koa()
 app.use(cors())
@@ -9,5 +10,14 @@ app.use(router.allowedMethods())
 
 const PORT = 3000
 app.listen(PORT, () => {
-    console.log(`Server is running and available at http://localhost:${PORT}/` )
+    console.log(`Server is running and available at http://localhost:${PORT}/`)
+    connectRedis().then(() => {
+        console.log("Redis is connected. Initialize Data...")
+        return initializeData("resources/worldcities.csv")
+    }).then(() => {
+        console.log("Initialization completed")
+    })
 })
+
+
+
