0fdc4097fb9dbb971e33e565c4e6f440e39904f4
Kaufmann Dario <dario.kaufmann@students.unibe.ch>
2024-12-15 12:08:52 +0100

    debug env var

--- news-map/src/services/api/urlConfig.ts
+++ news-map/src/services/api/urlConfig.ts
@@ -1,5 +1,6 @@
 
 const host = process.env.REACT_APP_API_HOST || 'localhost'
 const port = process.env.REACT_APP_API_PORT || '3000'
+console.log(process.env.REACT_APP_API_HOST, process.env.REACT_APP_API_PORT )
 export const baseURL = `http://${host}:${port}`
 
