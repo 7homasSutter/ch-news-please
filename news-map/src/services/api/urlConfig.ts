8ddc16d34cd7a90846f710801137d730e3576dcf
Kaufmann Dario <dario.kaufmann@students.unibe.ch>
2024-12-15 13:39:05 +0100

    changed to vite env variables

--- news-map/src/services/api/urlConfig.ts
+++ news-map/src/services/api/urlConfig.ts
@@ -1,6 +1,6 @@
 
-const host = process.env.REACT_APP_API_HOST || 'localhost'
-const port = process.env.REACT_APP_API_PORT || '3000'
-console.log(process.env.REACT_APP_API_HOST, process.env.REACT_APP_API_PORT)
+const host = import.meta.env.VITE_API_HOST || 'localhost'
+const port = import.meta.env.VITE_API_PORT || '3000'
+console.log(import.meta.env.VITE_API_HOST, import.meta.env.VITE_API_PORT)
 export const baseURL = `http://${host}:${port}`
 
