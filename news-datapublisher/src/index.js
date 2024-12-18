4c0c245e2a10abf9733405780a2e2c112905cc3c
Kaufmann Dario <dario.kaufmann@students.unibe.ch>
2024-12-18 12:06:34 +0100

    create https server

--- news-datapublisher/src/index.js
+++ news-datapublisher/src/index.js
@@ -2,6 +2,12 @@ import Koa from 'koa'
 import cors from "@koa/cors"
 import {router} from "./api/routes.js";
 import {connectRedis, initializeData} from "./services/redisService.js";
+import fs from "fs";
+import https from 'https'
+import dotenv from "dotenv";
+
+dotenv.config()
+
 
 const app = new Koa()
 app.use(cors({
@@ -11,15 +17,37 @@ app.use(router.routes())
 app.use(router.allowedMethods())
 
 const PORT = 3000
-app.listen(PORT, () => {
-    console.log(`Server is running and available at http://localhost:${PORT}/`)
+
+const pathToCert = process.env.CERT
+const pathToPrivateKey = process.env.PRIVATE_KEY
+
+if (!pathToCert || !pathToPrivateKey) {
+    console.log("Please provide a certificate and a private key to enable https.")
+    app.listen(PORT, () => {
+        handleServerStart(false)
+    })
+} else {
+    const options = {
+        key: fs.readFileSync(pathToPrivateKey),
+        cert: fs.readFileSync(pathToCert)
+    }
+    https.createServer(options, app.callback()).listen(PORT, () => {
+        handleServerStart(true)
+    })
+}
+
+
+function handleServerStart(isHttps) {
+    console.log(`Server is running and available at ${isHttps? 'https' : 'http'}://localhost:${PORT}/`)
     connectRedis().then(() => {
         console.log("Redis is connected. Initialize Data...")
         return initializeData("resources/worldcities.csv")
     }).then(() => {
         console.log("Initialization completed")
     })
-})
+}
+
+
 
 
 
